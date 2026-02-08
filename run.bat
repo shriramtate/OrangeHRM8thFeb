f42290a85ef893881e8cf2beb19bb8798223cacb
shriramtate <sramtate.fipdiab@gmail.com>
2026-02-08 18:39:49 +0530

    Update pytest report filename in run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
-pytest -v --browser chrome --html=Reports/orangeHRM_login_015.html --alluredir=Allure_Reports8thFeb
+pytest -v --browser chrome --html=Reports/orangeHRM_login_018.html --alluredir=Allure_Reports8thFeb
+
 
